18cb2ac40d7b005b334681b559c0079999b78c6d
chandni <chandni.rajendran@gmail.com>
2020-10-16 01:10:52 +0200

    Moved from p5.js web editor

--- sketch.js
+++ sketch.js
@@ -1,8 +1,36 @@
-function setup() {
+let t;
+
+function setup() { 
   createCanvas(windowWidth, windowHeight);
+  background(360);
+} 
+
+function draw() { 
+  t = frameCount;
+  colorMode(HSL, 360);
+  translate(width/2, height/2);
+   
+  noStroke();
+  strokeWeight(1);
+  background(360);
+  
+  
+  a = 100;
+  b = 150;
+  c = 300;
+  d = 360;
+  
+  //   make a set for  'grain' number of rings
+  let grain = 50;
+  for (x = 0; x<grain; x++){
+    hue   = map(x, 0, grain, 360,0);
+    fill(hue, 360, 150, 60);
+    // ellipse( a, b, c, d);
+    //replace ellipse with equation of x,y
+    bezier( 0, 0, 60*sin(t/500), 700 *sin(t/200), 800*cos(t/200), 60 *cos(t/50), 0, 0);
+    rotate(2*PI/grain);
+  }
 }
 
-function draw() {
-  background(220);
-  circle(20, 20, 20);
-}
+
+
